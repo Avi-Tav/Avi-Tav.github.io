72224cc8a51549ca98af9931b72e58e651056642
Avi T <a.tavdish@gmail.com>
2020-12-26 16:00:24 +0200

    fixes

--- my-portfolio/dist/js/script.js
+++ my-portfolio/dist/js/script.js
@@ -1,13 +1,13 @@
 
 
 //const work1Link = document.querySelector(".work1-link");
-const work1Language = document.querySelector(".work1-language");
+//const work1Language = document.querySelector(".work1-language");
 const work1Name = document.querySelector(".work1-name");
 const work1Description = document.querySelector(".work1-description");
 const work1Stars = document.querySelector(".work1-stargazers_count");
 
 //const work2Link = document.querySelector(".work2-link");
-const work2Language = document.querySelector(".work2-language");
+//const work2Language = document.querySelector(".work2-language");
 const work2Name = document.querySelector(".work2-name");
 const work2Description = document.querySelector(".work2-description");
 const work2Stars = document.querySelector(".work2-stargazers_count");
@@ -23,21 +23,22 @@ fetch('https://api.github.com/users/Avi-Tav/repos')
   })
   .then(function (data) {
     appendData(data);
+    console.log(data);
   })
   .catch(function (err) {
     console.log(err);
   });
   
   function appendData(data) {
-    work1Name.innerHTML = `${data[2].name}`;
-    work1Language.innerHTML = `${data[2].language}`;
-    work1Description.innerHTML = `${data[2].description}`;
-    work1Stars.innerHTML = `&#9734 ${data[2].stargazers_count}`;
+    work1Name.innerHTML = `${data[1].name}`;
+    //work1Language.innerHTML = `${data[1].language}`;
+    work1Description.innerHTML = `${data[1].description}`;
+    work1Stars.innerHTML = `&#9734 ${data[1].stargazers_count}`;
     //work1Link.innerHTML = `${data[2].clone_url}`;
 
-    work2Name.innerHTML = `${data[1].name}`;
-    work2Language.innerHTML = `${data[1].language}`;
-    work2Description.innerHTML = `${data[1].description}`;
-    work2Stars.innerHTML = `&#9734 ${data[1].stargazers_count}`;
+    work2Name.innerHTML = `${data[0].name}`;
+    //work2Language.innerHTML = `${data[0].language}`;
+    work2Description.innerHTML = `${data[0].description}`;
+    work2Stars.innerHTML = `&#9734 ${data[0].stargazers_count}`;
     //work2Link.innerHTML = `${data[1].clone_url}`;
 }
