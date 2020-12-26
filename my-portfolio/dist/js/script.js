9f67e699d4f23fa998f49320d876e0ed54031244
Avi T <a.tavdish@gmail.com>
2020-12-26 11:27:33 +0200

    Fixing decoration
    Fixing decoration

--- my-portfolio/dist/js/script.js
+++ my-portfolio/dist/js/script.js
@@ -1,18 +1,27 @@
-let btnDownload = document.querySelector('button');
+const work1Link = document.querySelector(".work1-link");
+const work1Language = document.querySelector(".work1-language");
+const work1Name = document.querySelector(".work1-name");
+const work1Description = document.querySelector(".work1-description");
+const work1Stars = document.querySelector(".work1-stargazers_count");
 
-function download(){ 
-    axios({ 
-        url:'https://source.unsplash.com/random/500x500', 
-        method:'GET', 
-        responseType: 'blob' 
-}) 
-.then((response) => { 
-       const url = window.URL 
-       .createObjectURL(new Blob([response.data])); 
-              const link = document.createElement('a'); 
-              link.href = url; 
-              link.setAttribute('download', 'image.jpg'); 
-              document.body.appendChild(link); 
-              link.click(); 
-}) 
-}
+
+const client_id = "Iv1.89313f83851fa72e";
+const client_secret = "b69e61f49e7844d039c01d2f3521df9eb9e5d357";
+
+// const fetchUser = () => {
+//     const api_call = fetch(`https://api.github.com/users/Avi-Tav/repos${user}?client_is=${client_id}&client_secret=${client_secret}`);
+
+// const data = api_call.json();
+// return { data }
+// }
+
+// const showData = () => {
+//     fetchUser(inputValue.value).then((res) => {
+//         console.log(res);
+//     })
+// };
+
+fetch('https://api.github.com/users/Avi-Tav/repo')
+    .then(response => response.json())
+		.then(data => document.body.append())
+		.catch(error => console.error(error));
